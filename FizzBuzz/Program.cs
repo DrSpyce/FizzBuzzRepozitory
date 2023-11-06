0b08f29bc39a78d660383323085967c674071028
DrSpyce <ikozakov2018@gmail.com>
2023-11-06 16:26:41 +0100

    feat(fizzbuzz): FizzBuzz class added

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,41 +1,14 @@
-﻿using System;
+﻿using FizzBuzz;
+using System;
 
-class FizzBuzz
+class Program
 {
-    enum FizzBuzzConstants
-    {
-        Fizz,
-        Buzz,
-        Whizz
-    }
-
     static void Main()
     {
         for (int i = 1; i <= 100; i++)
         {
-            Console.WriteLine(GetFizzBuzz(i));
+            Console.WriteLine(FizzBuzzClass.GetFizzBuzz(i));
         }
     }
 
-    public static string GetFizzBuzz(int number)
-    {
-        string result = string.Empty;
-
-        if (IsDivisibleByThree(number))
-            result += FizzBuzzConstants.Fizz;
-        if (IsDivisibleByFive(number))
-            result += FizzBuzzConstants.Buzz;
-        if (IsDivisibleBySeven(number))
-            result += FizzBuzzConstants.Whizz;
-        if (result.Length == 0)
-            result += number.ToString();
-
-        return result;
-    }
-
-    public static bool IsDivisibleByThree(int num) => num % 3 == 0;
-
-    public static bool IsDivisibleByFive(int num) => num % 5 == 0;
-
-    public static bool IsDivisibleBySeven(int num) => num % 7 == 0;
 }
